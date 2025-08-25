522288cbb0cb2a243e9ae74e3e9479aa3a9d8078
Manas Malla <manasmalla.dev@gmail.com>
2025-08-26 04:35:33 +0530

    added changes
    Signed-off-by: Manas Malla <manasmalla.dev@gmail.com>

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
+console.log("Document loaded");
+// Change the countdown timer every 1 second
+var countdownDate = new Date("Aug 26, 2025 17:00:00").getTime();
+var x = setInterval(function () {
+  var now = new Date().getTime();
+  var distance = countdownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Output the result in an element with id="demo"
+  document.getElementById("countdown").innerHTML =
+    hours + "h " + minutes + "m " + seconds + "s ";
+
+  // If the count down is over, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
 function sendMailForDeletion() {
   console.log("Sending email for deletion...");
   window.location.href =
